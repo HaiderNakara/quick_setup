10edeb1b553a87a47e24fdd2ee122f752b292e9f
haider <haider.n@somaiya.edu>
2022-07-10 13:47:37 +0530

    nestjs rest completed

--- dist/nest_fn.js
+++ dist/nest_fn.js
@@ -52,7 +52,7 @@ function createNestJs(name) {
 exports.createNestJs = createNestJs;
 function create_nestjs(name) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield exec_run(`cd ${name} && npx @nestjs/cli new ${name}_backend --package-manager npm && cd ${name}_backend && npm install @nestjs/mongoose mongoose @nestjs/swagger swagger-ui-express`);
+        yield exec_run(`cd ${name} && npx @nestjs/cli new ${name}_backend --package-manager npm && cd ${name}_backend && npm install @nestjs/mongoose mongoose @nestjs/swagger swagger-ui-express --legacy-peer-deps && npm i --save @nestjs/config --legacy-peer-deps && cd src && mkdir entities dto`);
         yield fs.writeFile(`${name}/${name}_backend/src/main.ts`, `import { NestFactory } from '@nestjs/core';
    import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
    import { AppModule } from './app.module';
@@ -71,6 +71,161 @@ function create_nestjs(name) {
    }
    bootstrap();
    `);
+        yield fs.writeFile(`${name}/${name}_backend/src/app.module.ts`, `import { Module } from '@nestjs/common';
+   import { AppController } from './app.controller';
+   import { AppService } from './app.service';
+   import { ConfigModule } from '@nestjs/config';
+   import { MongooseModule } from '@nestjs/mongoose';
+   import { Cat, CatSchema } from './entities/app.entity';
+   
+   @Module({
+     imports: [
+       ConfigModule.forRoot({
+         isGlobal: true,
+         envFilePath: '.env',
+       }),
+       MongooseModule.forRoot(process.env.MONGO_URI),
+       MongooseModule.forFeature([
+         { name: Cat.name, schema: CatSchema },
+       ]),
+     ],
+     controllers: [AppController],
+     providers: [AppService],
+   })
+   export class AppModule {}
+   `);
+        yield fs.writeFile(`${name}/${name}_backend/src/app.controller.ts`, `import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+    import { AppService } from './app.service';
+    import { UpdateCatDto } from './dto/update-cat.dto';
+    import { Cat } from './entities/app.entity';
+    
+    @Controller()
+    export class AppController {
+      constructor(private readonly appService: AppService) {}
+    
+      @Get()
+      getHello(): string {
+        return this.appService.getHello();
+      }
+      @Post('/cats/')
+        createCat( @Body() createCatDto: Cat) {
+        return this.appService.createCat(createCatDto);
+      }
+      @Get('/cats/')
+      findAllCats() {
+        return this.appService.findAllCats();
+      }
+      @Get('/cats/:id')
+      findCatById(@Param('id') id: string) {
+        return this.appService.findCatById(id);
+      }
+      @Patch('/cats/:id')
+      updateCat(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return this.appService.updateCat(id, updateCatDto);
+      }
+      @Delete('/cats/:id')
+      deleteCat(@Param('id') id: string) {
+        return this.appService.deleteCat(id);
+      }
+    }`);
+        yield fs.writeFile(`${name}/${name}_backend/src/app.service.ts`, `import { Injectable } from '@nestjs/common';
+    import { InjectModel } from '@nestjs/mongoose';
+    import { Model } from 'mongoose';
+    import { UpdateCatDto } from './dto/update-cat.dto';
+    import { Cat } from './entities/app.entity';
+    
+    @Injectable()
+    export class AppService {
+      constructor(
+      @InjectModel(Cat.name) private readonly catModel: Model<Cat>,
+      ) {} 
+      getHello(): string {
+        return 'Hello World!';
+      }
+      async createCat(cat: Cat): Promise<Cat> {
+        const createdCat = new this.catModel(cat);
+        return await createdCat.save();
+      }
+      async findAllCats(): Promise<Cat[]> {
+        return await this.catModel.find().exec();
+      }
+    async findCatById(id: string): Promise<Cat> {
+      return await this.catModel.findById(id).exec();
+    }
+    async updateCat(id: string, cat: UpdateCatDto): Promise<Cat> {
+      return await this.catModel.findByIdAndUpdate(id, cat, { new: true }).exec();
+    } 
+    async deleteCat(id: string): Promise<Cat> {
+      return await this.catModel.findByIdAndDelete(id).exec();
+    }
+    }
+    `);
+        yield fs.writeFile(`${name}/${name}_backend/src/entities/app.entity.ts`, `import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+      import { ApiProperty } from "@nestjs/swagger";
+      import { Document } from 'mongoose';
+      
+      @Schema()
+      export class Cat extends Document {
+        @ApiProperty()
+        @Prop()
+        name: string;
+        @ApiProperty()
+        @Prop()
+        age: number;
+        @ApiProperty()
+        @Prop()
+        breed: string;
+        @ApiProperty()
+        @Prop()
+        color: string;
+        @ApiProperty()
+        @Prop()
+        weight: number;
+      }
+      
+      export const CatSchema = SchemaFactory.createForClass(Cat);`);
+        yield fs.writeFile(`${name}/${name}_backend/src/dto/update-cat.dto.ts`, `import { PartialType } from "@nestjs/swagger";
+        import { Cat } from "src/entities/app.entity";
+        
+        export class UpdateCatDto extends PartialType(Cat) {}     
+        `);
+        yield fs.writeFile(`${name}/${name}_backend/.env`, `MONGO_URI=Your_Mongo_URI`);
+        yield fs.writeFile(`${name}/${name}_backend/src/.gitignore`, `.env
+        # compiled output
+        /dist
+        /node_modules
+        
+        # Logs
+        logs
+        *.log
+        npm-debug.log*
+        pnpm-debug.log*
+        yarn-debug.log*
+        yarn-error.log*
+        lerna-debug.log*
+        
+        # OS
+        .DS_Store
+        
+        # Tests
+        /coverage
+        /.nyc_output
+        
+        # IDEs and editors
+        /.idea
+        .project
+        .classpath
+        .c9/
+        *.launch
+        .settings/
+        *.sublime-workspace
+        
+        # IDE - VSCode
+        .vscode/*
+        !.vscode/settings.json
+        !.vscode/tasks.json
+        !.vscode/launch.json
+        !.vscode/extensions.json`);
     });
 }
 exports.create_nestjs = create_nestjs;
